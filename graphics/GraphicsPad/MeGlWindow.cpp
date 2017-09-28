89edbef2070af8dfcac1a81e13623b96355f0efc
Andrew Liu <u1089896@ad.utah.edu>
2017-09-28 16:05:35 -0600

    Finished tutorial 36. Added ShapeData class, able to render cube.

--- graphics/GraphicsPad/MeGlWindow.cpp
+++ graphics/GraphicsPad/MeGlWindow.cpp
@@ -3,17 +3,21 @@
 #include <glm\gtx\transform.hpp>
 #include <MeGlWindow.h>
 #include <Vertex.h>
+#include <ShapeGenerator.h>
 #include <iostream>
 #include <fstream>
 
 using namespace std;
+using glm::vec3;
+using glm::mat4;
 
 const uint NUM_VERTICES_PER_TRI = 3;
 const uint NUM_FLOATS_PER_VERTICE = 6;
 const uint VERTEX_BYTE_SIZE = NUM_FLOATS_PER_VERTICE * sizeof(float);
 GLuint programID;
+GLuint indices;
 
-glm::vec3 position;
+vec3 position;
 float angle = 0.0f;
 float scaling = 1.0f;
 bool isBig = true;
@@ -59,47 +63,95 @@ void installShaders()
 
 void sendContent()
 {
-	// Vertex positions on the screen
-	Vertex triPos[] = {
-		glm::vec3(+0.0f, +1.0f, +1.0f),
-		glm::vec3(+1.0f, +0.0f, +0.0f),
+	//// Vertex positions on the screen
+	//Vertex triPos[] = {
+	//	glm::vec3(+0.0f, +1.0f, +1.0f),
+	//	glm::vec3(+1.0f, +0.0f, +0.0f),
 
-		glm::vec3(-1.0f, -1.0f, +1.0f),
-		glm::vec3(+0.0f, +1.0f, +0.0f),
-		
-		glm::vec3(+1.0f, -1.0, +1.0f),
-		glm::vec3(+0.0f, +0.0f, +1.0f),
-	};
+	//	glm::vec3(-1.0f, -1.0f, +1.0f),
+	//	glm::vec3(+0.0f, +1.0f, +0.0f),
+	//	
+	//	glm::vec3(+1.0f, -1.0, +1.0f),
+	//	glm::vec3(+0.0f, +0.0f, +1.0f),
+	//};
 
-	// Order of drawing
-	static GLushort indices[] = {
-		0, 1, 2,
-	};
+	//// Order of drawing
+	//static GLushort indices[] = {
+	//	0, 1, 2,
+	//};
+
+	ShapeData cube = ShapeGenerator::makeCube();
 	
-	GLuint bufferID;
-	glGenBuffers(1, &bufferID);
-	glBindBuffer(GL_ARRAY_BUFFER, bufferID);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, bufferID);
-
-	glBufferData(GL_ARRAY_BUFFER, sizeof(triPos) + sizeof(indices), 0, GL_STATIC_DRAW);
-	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(triPos), triPos);
-	glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, sizeof(triPos), sizeof(indices), indices);
-
+	GLuint vertexBufferID;
+	glGenBuffers(1, &vertexBufferID);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+	glBufferData(GL_ARRAY_BUFFER, cube.vertexBufferSize(), cube.vertices, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
 	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (char*)(sizeof(float) * 3));
+
+	GLuint indexArrayBufferID;
+	glGenBuffers(1, &indexArrayBufferID);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexArrayBufferID);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, cube.indexBufferSize(), cube.indices, GL_STATIC_DRAW);
+
+	//GLuint bufferID;
+	//glGenBuffers(1, &bufferID);
+	//glBindBuffer(GL_ARRAY_BUFFER, bufferID);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, bufferID);
+
+	//glBufferData(GL_ARRAY_BUFFER, cube.vertexBufferSize() + cube.indexBufferSize(), cube.vertices, GL_STATIC_DRAW);
+	//glBufferSubData(GL_ARRAY_BUFFER, 0, cube.vertexBufferSize(), cube.vertices);
+	//glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, cube.vertexBufferSize(), cube.indexBufferSize(), cube.indices);
+
+	//glEnableVertexAttribArray(0);
+	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
+	//glEnableVertexAttribArray(1);
+	//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (char*)(sizeof(float) * 3));
+
+	indices = cube.numIndices;
+
+	cube.cleanup();
 }
 
 void transformShape()
 {	
-	glm::mat4 rotate = glm::rotate(angle, 0.0f, 0.0f, 1.0f);
-	glm::mat4 translate = glm::translate(position);
-	glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(scaling));
+	mat4 rotate = glm::rotate(angle, 0.0f, 0.0f, 1.0f);
+	mat4 translate = glm::translate(position);
+	mat4 scale = glm::scale(mat4(1.0f), vec3(scaling));
 
-	glm::mat4 transform = rotate * translate * scale;
+	mat4 transform = rotate * translate * scale;
 	GLint uniformLoc = glGetUniformLocation(programID, "transform");
 	glUniformMatrix4fv(uniformLoc, 1, GL_FALSE, &transform[0][0]);
+}
+
+void MeGlWindow::initializeGL()
+{
+	glewInit();
+	glEnable(GL_DEPTH_TEST);
+	installShaders();
+	sendContent();
+}
+
+void MeGlWindow::paintGL()
+{
+	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+	glViewport(0, 0, width(), height());
+
+	mat4 modelTransformMatrix = glm::translate(mat4(), vec3(0.0f, 0.0f, -3.0f));
+	mat4 projectionMatrix = glm::perspective(60.0f, ((float)width()) / height(), 0.1f, 10.0f);
+
+	GLint modelTransMatUniformLoc = glGetUniformLocation(programID, "modelTransMat");
+	GLint projectionMatUniformLoc = glGetUniformLocation(programID, "projectionMat");
+
+	glUniformMatrix4fv(modelTransMatUniformLoc, 1, GL_FALSE, &modelTransformMatrix[0][0]);
+	glUniformMatrix4fv(projectionMatUniformLoc, 1, GL_FALSE, &projectionMatrix[0][0]);
+
+	transformShape();
+
+	// (void*) number of vertex position floats * 4
+	glDrawElements(GL_TRIANGLES, indices, GL_UNSIGNED_SHORT, 0);
 }
 
 void MeGlWindow::keyPressEvent(QKeyEvent* e)
@@ -151,21 +203,3 @@ void MeGlWindow::keyPressEvent(QKeyEvent* e)
 	}
 	repaint();
 }
-
-void MeGlWindow::initializeGL()
-{
-	glewInit();
-	glClearColor(0, 0, 0, 1);
-	installShaders();
-	sendContent();
-}
-
-void MeGlWindow::paintGL()
-{
-	glClear(GL_COLOR_BUFFER_BIT);
-	glViewport(0, 0, width(), height());
-	transformShape();
-	// (void*) number of vertex position floats * 4
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, (void*)72);
-}
-
--- graphics/GraphicsPad/VertexShader.glsl
+++ graphics/GraphicsPad/VertexShader.glsl
@@ -4,11 +4,16 @@ in layout(location=0) vec3 position;
 in layout(location=1) vec3 vertexColor;
 
 uniform mat4 transform;
+uniform mat4 modelTransMat;
+uniform mat4 projectionMat;
 
 out vec3 tempColor;
 
 void main()
 {	
-	gl_Position = transform * vec4(position, 1.0);
+	vec4 v = vec4(position, 1.0);
+	vec4 newPos = modelTransMat * v;
+	vec4 projectedPos = projectionMat * newPos;
+	gl_Position = transform * projectedPos;
 	tempColor = vertexColor;
 }
